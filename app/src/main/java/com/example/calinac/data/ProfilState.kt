fb09d896a0c0cb40c4d7a6e76b696f4a70dd3bb3
Alexandre Thomassin <alex45thomassin@gmail.com>
2024-03-21 10:47:32 +0100

    Update ProfilState.kt

--- app/src/main/java/com/example/calinac/data/ProfilState.kt
+++ app/src/main/java/com/example/calinac/data/ProfilState.kt
@@ -1,6 +1,16 @@
 package com.example.calinac.data
 
 data class ProfilState(
+
+    // Infos
     val username: String = "",
-    val 
+    val email: String = "",
+    val name: String = "",
+    val surName: String = "",
+    val phoneNumber: String = "",
+
+    // Bank infos
+    val rib: String = "",
+    val bank: String = "",
+
 )
