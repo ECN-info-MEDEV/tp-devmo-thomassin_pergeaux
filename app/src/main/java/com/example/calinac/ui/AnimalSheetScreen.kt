1ec87409266bddb710d522b89a1c9f5fc51fb495
Alexandre Thomassin <alex45thomassin@gmail.com>
2024-03-25 20:29:43 +0100

    Update App

--- app/src/main/java/com/example/calinac/ui/AnimalSheetScreen.kt
+++ app/src/main/java/com/example/calinac/ui/AnimalSheetScreen.kt
@@ -1,2 +1,14 @@
 package com.example.calinac.ui
 
+import androidx.compose.runtime.Composable
+import com.example.calinac.data.ProfilState
+
+// Dans le cadre du TP, on ne vas travailler qu'avec un seul animal, on ne va donc pas créer de BDD
+
+@Composable
+fun AnimalSheet(
+    state : AnimalState,
+    profilModel: AnimalViewModel
+){
+
+}
---
+++ app/src/main/java/com/example/calinac/ui/AnimalViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.calinac.ui
+
+class AnimalViewModel {
+}
